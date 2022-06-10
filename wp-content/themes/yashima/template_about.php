dfeda68216c64d5c478582dd1a23bbabf4be60e6
Ryan Watson <ryan.watson@clickmark.co.jp>
2022-06-10 17:16:38 +0900

    Custom fields for about template

--- wp-content/themes/yashima/template_about.php
+++ wp-content/themes/yashima/template_about.php
@@ -4,12 +4,11 @@
 
     <div class="container-lg text-center">
 
-    	<h1>八嶋組について</h1>
+    	<h1><?php the_title(); ?></h1>
 	
     </div><!-- /container -->
 
 </div><!-- /main banner -->
-
 
 <section class="py-5">
 	
@@ -17,28 +16,9 @@
 	
 		<div class="text-center">
 		
-			<h1 class="text-yashima-primary mb-5">一人ひとりの連携、未来に残る仕事</h1>
+			<h1 class="text-yashima-primary mb-5"><?php echo get_field('title'); ?></h1>
 			
-			<p>
-				建築、土木の仕事というのは、<br>
-				決して1人で出来るものではありません。<br>
-				施工管理、計測、計測データの集計、書類作成、<br>
-				機械・作業車の操作、現場の作業、<br>
-				様々な役割を担う人が居て初めて成し遂げられるものです。
-
-			</p>
-			
-			<p>
-				ある護岸の修繕工事をした時のことです。<br>
-				八嶋組創業者である私の祖父の名前が記された銘板を見つけました。<br>
-				それを修復し、また後の世代に繋いでいく…。
-			</p>
-			
-			<p>
-				今の仕事が十年、百年以上先の未来まで残る、
-				誇りのある、責任ある仕事をこれからも。
-			</p>			
-			
+			<?php echo get_field('content'); ?>
 			
 		</div><!-- /text center -->
 		
@@ -46,13 +26,13 @@
 		
 			<div class="col">
 				<div class="text-end">
-					<h5>代表取締役</h5>
+					<h5><?php echo get_field('role'); ?></h5>
 				</div><!-- /text end -->
 			</div>	
 			
 			<div class="col">
 				<div class="text-start">
-					<h4 class="text-yashima-primary">八嶋 秀和</h4>
+					<h4 class="text-yashima-primary"><?php echo get_field('name'); ?></h4>
 				</div><!-- /text start -->
 			</div>
 			
@@ -78,118 +58,24 @@
 			<div class="col-xl-8 m-auto col-lg-9 col-12">
 			
 				<table class="table mt-4">
-		
-					<tr>
-						<td>会社名</td>
-						<td>株式会社 八嶋組</td>
-					</tr>
-
-					<tr>
-						<td>本　社</td>
-						<td>〒656-1731<br>兵庫県淡路市仁井2番地3</td>
-					</tr>
 					
-					<tr>
-						<td>アクセスマップ</td>
-						<td>
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13147.55204267775!2d134.93959069696044!3d34.53106518165148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3554cc6a7cc9a6b5%3A0x65d2e5b1c23d9580!2z44CSNjU2LTE3MzEg5YW15bqr55yM5reh6Lev5biC5LuB5LqV77yS!5e0!3m2!1sja!2sjp!4v1626923438848!5m2!1sja!2sjp" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-						</td>
-					</tr>
+					<?php if( have_rows('company_table') ): ?>
+
+						<?php while( have_rows('company_table') ): the_row(); ?>
+						<tr>
+							<td><?php echo get_sub_field('label'); ?></td>
+							<td><?php echo get_sub_field('description'); ?></td>
+						</tr>
+						<?php endwhile; ?>
+
+					<?php endif; ?>
 					
-				
-					<tr>
-						<td>取締役社長</td>
-						<td>八嶋 秀和 <small>(Yashima Hidekazu)</small></td>
-					</tr>
-
-					<tr>
-						<td>TEL</td>
-						<td>0799-82-0600</td>
-					</tr>
-
-					<tr>
-						<td>FAX</td>
-						<td>0799-82-1295</td>
-					</tr>
-                    
-                    <tr>
-						<td>創　業</td>
-						<td>昭和17年4月</td>
-					</tr>
-                    
-					<tr>
-						<td>設　立</td>
-						<td>昭和48年3月</td>
-					</tr>
-
-					<tr>
-						<td>資本金</td>
-						<td>2,000万円</td>
-					</tr>
-
-
-					<tr>
-						<td>年　商</td>
-						<td>約8億5000万円 (2020月6月決算)</td>
-					</tr>
-
-					<tr>
-						<td>従業員</td>
-						<td>15人(2020年4月1日現在)</td>
-					</tr>
-
-					<tr>
-						<td>事業内容</td>
-						<td>土木・建築等建設工事の請負(総合建設業)</td>
-					</tr>
-
-					<tr>
-						<td>許可種別</td>
-						<td>
-							建設業許可：兵庫県知事許可<br>
-                            （特－28）第800842号<br>
-                            土木工事事業／とび・土工工事事業／舗装工事業／水道施設工事業／解体工事業<br>
-							（般－28）第800842号<br>
-                            建築工事業／管工事業／造園工事業<br>
-						</td>
-					</tr>
-                    <tr>
-						<td>登録</td>
-						<td>
-							エコアクション21<br>認証・登録番号0010393
-						</td>
-					</tr>
-
-					
-							
-					<tr>
-						<td>有資格者</td>
-						<td>
-							1級土木施工管理技士　　　8人<br>
-							1級機械施工管理技士　　　1人<br>
-							2級土木施工管理技士　　　3人<br>
-							1級造園施工管理技士　　　1人<br>
-							2級建築施工管理技士　　　3人<br>
-							給水装置工事主任技術者　    2人<br>　
-						</td>
-					</tr>
-
-					<tr>
-						<td>加入団体</td>
-						<td>一般社団法人兵庫県建設業協会</td>
-					</tr>
-
-					<tr>
-						<td>主要取引銀行</td>
-						<td>淡路信用金庫／淡陽信用組合／みなと銀行</td>
-					</tr>
 				</table>
 
 				
 			</div><!-- /col 9 -->
 			
 		</div><!-- /row -->
-		
 		
 	</div><!-- /container -->
 	
@@ -212,104 +98,16 @@
 
                 <table class="table mt-4">
 
-                    <tr>
-                        <td>1942年 (昭和17年) 4月</td>
-                        <td>
-                            八嶋組　創業
-                        </td>
-                    </tr>
+                    <?php if( have_rows('history_table') ): ?>
 
-                    <tr>
-                        <td>1951年（昭和26年）8月</td>
-                        <td>
-                            特定建設業許可　土木、舗装、水道施設工事業　取得
-                        </td>
-                    </tr>
+						<?php while( have_rows('history_table') ): the_row(); ?>
+							<tr>
+								<td><?php echo get_sub_field('label'); ?></td>
+								<td><?php echo get_sub_field('details'); ?></td>
+							</tr>
+						<?php endwhile; ?>
 
-                    <tr>
-                        <td>1955年（昭和30年）4月</td>
-                        <td>
-                            社団法人兵庫県建設業協会　所属
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>1973年（昭和48年）3月</td>
-                        <td>
-                            株式会社八嶋組　設立<br>
-                            八嶋三吉　代表取締役　就任<br>
-                            資本金　600万円<br>
-                            本社　兵庫県津名郡北淡町野島平林58番地
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>1979年（昭和54年）8月</td>
-                        <td>
-                            八嶋正信　代表取締役　就任
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>1985年（昭和60年）10月</td>
-                        <td>
-                            資本金　1,000万円に増資
-                        </td>
-                    </tr>
-					
-					<tr>
-                        <td>1988年（昭和63年）9月</td>
-                        <td>
-                            資本金　1,500万円に増資
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>1997年（平成9年）2月</td>
-                        <td>
-                            一般建設業許可　建築工事業　取得
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>1997年（平成9年）6月</td>
-                        <td>
-                            資本金　2,000万円に増資
-                        </td>
-                    </tr>
-                    
-                    <tr>
-                        <td>2001年（平成13年）12月</td>
-                        <td>
-                            特定建設業許可　とび・土工工事業　取得<br>
-							一般建設業許可　管工事業　取得
-                        </td>
-                    </tr>
-					<tr>
-                        <td>2011年（平成23年）6月</td>
-                        <td>
-                            一般建設業許可　造園工事業　取得
-                        </td>
-                    </tr>
-					<tr>
-                        <td>2015年（平成27年）1月</td>
-                        <td>
-                            エコアクション21　認証
-                        </td>
-                    </tr>
-					<tr>
-                        <td>2016年（平成28年）11月</td>
-                        <td>
-                            特定建設業許可　解体工事業　取得
-                        </td>
-                    </tr>
-					<tr>
-                        <td>2017年（平成29年）4月</td>
-                        <td>
-                            八嶋秀和　代表取締役　就任<br>
-							本社移転　兵庫県淡路仁井2番地3
-                        </td>
-                    </tr>
+					<?php endif; ?>
 
                 </table>
 
@@ -317,7 +115,6 @@
 
         </div><!-- /row -->
 			
-		
 	</div><!-- /container -->
 	
 </section>
@@ -339,64 +136,16 @@
 
                 <table class="table mt-4">
 
-                    <tr>
-                        <td>2012年</td>
-                        <td>
-                       育波川水系 育波川河川災害復旧工事<br>由良港防波堤（南）防食対策工事<br>生穂新島水源整備工事
-                        </td>
-                    </tr>
+                    <?php if( have_rows('works_table') ): ?>
 
-                    <tr>
-                        <td>2013年</td>
-                        <td>
-                        五色浜海岸 人工リーフ設置工事<br>佐野仁井岩屋線 道路改修工事（A工区）<br>沼島漁港沖2号防波堤改良工事<br>西濱水産㈱加工場新築工事<br>淡路SA上り線メイントイレ工事
-                        </td>
-                    </tr>
+						<?php while( have_rows('works_table') ): the_row(); ?>
+							<tr>
+								<td><?php echo get_sub_field('label'); ?></td>
+								<td><?php echo get_sub_field('description'); ?></td>
+							</tr>
+						<?php endwhile; ?>
 
-                    <tr>
-                        <td>2014年</td>
-                        <td>
-                          内田海岸消波堤設置工事<br>尾崎漁港護岸改良工事<br>県立淡路高等学校用地ため池改修工事
-
-                        </td>
-                    </tr>
-                  
-                    <tr>
-                        <td>2015年</td>
-                        <td>
-                         沼島漁港沖2号防波堤改良工事（その３）<br>郡家川水系郡家川河川災害復旧工事<br>津名港津名水叩き舗装工事<br>志筑川水系志筑川大円道橋上部工工事<br>垂水第一料金所管理用通路設置工事
-
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>2016年</td>
-                        <td>
-                         沼島漁港沖2号防波堤改良工事（その4）<br>内田海岸消波堤設置工事（その３）<br>江井大井地区道路改良舗装工事（4工区）<br>市道地勝寺線排水管布設替工事<br>淡路地区予防治山事業（農山漁村）（28Y第16号）
-                        </td>
-                    </tr>
-
-
-                    <tr>
-                        <td>2017年</td>
-                        <td>
-                         洲本港沈下対策工事（１工区）<br>北淡海岸淡路市護岸補強工事（その３）
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>2018年</td>
-                        <td>
-                         仮屋漁港内防波堤ブロック製作工事<br>河上地区菱池・ニゴリ池改修工事<br>洲本港越流対策工事（その３）<br>茶間（3）急傾斜地崩壊対策工事<br>大芝生裏事務所トイレ新築工事
-                        </td>
-                    </tr>
-					
-					<tr>
-                        <td>2019年</td>
-                        <td>
-                         一宮海岸淡路市撫2工区離岸堤嵩上げ工事<br>御手洗池改修工事<br>仮屋漁港内防波堤ブロック設置工事<br>淡路島公園森のゾーン駐車場整備工事<br>旧北淡リサイクルセンター解体工事
-                        </td>
-                    </tr>
+					<?php endif; ?>
 
                 </table>
 
@@ -404,7 +153,6 @@
 
         </div><!-- /row -->
 			
-		
 	</div><!-- /container -->
 	
 </section>
